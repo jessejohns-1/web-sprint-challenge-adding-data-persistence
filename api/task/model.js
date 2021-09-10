b4659c919d1aad091dc1aaa50cd8bca831eda10f
Jesse <eoshout@Gmail.com>
2021-09-10 12:10:46 -0400

    a lot of work

--- api/task/model.js
+++ api/task/model.js
@@ -1,19 +1,45 @@
-// build your `Task` model here
 const db = require("../../data/dbConfig");
 
-function findTask() {
-    // const id = await db("tasks").insert(task);
-    // const foundTask = {
-    //     task_id: id,
-    //     task_description: task.task_description,
-    //     task_notes: task.task_notes,
-    //     task_completed: task.task_completed,
-    //     project_id: task.pro
-    // }
+async function insert(task) {
+  const [id] = await db("tasks").insert(task);
+  const newTask = {
+    task_id: id,
+    task_description: task.task_description,
+    task_notes: task.task_notes,
+    task_completed: task.task_completed,
+    project_id: task.project_id,
+  };
+  return newTask;
 }
 
-function postTasks() {
+async function getTasks() {
+  const results = await db("tasks as t")
+    .leftJoin("projects as p", "p.project_id", "t.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "t.task_notes",
+      "t.task_completed",
+      "p.project_name",
+      "p.project_description"
+    );
 
+  const tasks = [];
+  results.forEach((e) => {
+    tasks.push({
+      task_id: e.task_id,
+      task_description: e.task_description,
+      task_notes: e.task_notes,
+      task_completed: Boolean(e.task_completed),
+      project_name: e.project_name,
+      project_description: e.project_description,
+    });
+  });
+
+  return tasks;
 }
 
-module.exports = {  findTasks, postTasks,};
+module.exports = {
+  getTasks,
+  insert,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,33 @@
 const express = require("express");
+
 const router = express.Router();
+
 const Task = require("./model");
 
+router.get("/", (req, res, next) => {
+  Task.getTasks()
+    .then((task) => {
+      res.json(task);
+    })
+    .catch(next);
+});
+router.post("/", (req, res, next) => {
+  const data = req.body;
+  console.log(data)
+  Task.insert(data)
+    .then((newPro) => {
+      res.json(newPro);
+    })
+    .catch(next);
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    sageAdvice: "Finding the real error is 90% of the bug fix",
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = router;
